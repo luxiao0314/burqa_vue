365f1cb3446b2577c65b58b9d87bd0e0127e772b
xiao.lu <xiao.lu@magicwindow.cn>
2018-07-16 19:21:43 +0800

    commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,8 +8,8 @@ import { MessageBox } from 'mint-ui';
 NProgress.inc(0.2)
 NProgress.configure({ easing: 'ease', speed: 500, showSpinner: false })
 
-// axios.defaults.baseURL = '/api'
-axios.defaults.baseURL = 'https://app.u17.com'
+axios.defaults.baseURL = '/api'
+// axios.defaults.baseURL = 'https://app.u17.com'
 axios.defaults.timeout = 10000
 axios.defaults.headers = {
   'X-Requested-With': 'XMLHttpRequest',
